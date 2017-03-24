f3e45b2993f67f334f1eaa12005dd1e912d90c07
Jack Stephens <jack@jrcs3.com>
2017-03-23 20:43:31 -0700

    Base Functionality

---
+++ index.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var app = express();
+var figlet = require('figlet');
+var fs = require('fs');
+
+app.get('/', function(req, res) {
+
+    fs.readFile('./words.json', 'utf8', function (err, data) {
+        if (err) throw err;
+        var objw = JSON.parse(data);
+        var words = objw.words;
+        //console.log(words);
+        word = words[Math.floor(Math.random() * words.length)];
+        fs.readFile('./fonts.json', 'utf8', function (err, data) {
+            if (err) throw err;
+            var objf = JSON.parse(data);
+            var fonts = objf.fonts;
+            //console.log(words);
+            font = fonts[Math.floor(Math.random() * fonts.length)];
+
+
+            figlet.text(word, {'font': font,  'horizontalLayout': 'default',
+    'verticalLayout': 'default'}, function(err, data) {
+                if (err) throw err;
+                console.log(data);
+                var rVal = {};
+                rVal.word = word;
+                rVal.capta = data.split('\n');
+                res.json(rVal);
+            });
+        });
+    });
+
+
+
+});
+
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+    console.log('Listening on port #' + port);
+});
